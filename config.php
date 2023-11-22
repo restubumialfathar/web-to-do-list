8419cfc4776f4549877a16e44605ce474929afa7
restubumialfathar <restubumialfathar12@gmail.com>
2023-11-22 14:29:26 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$db="db_notepad";
+
+
+$conn = mysqli_connect($host,$user,$pass,$db);
+
+
+mysqli_select_db($conn,$db);
+?>
---
+++ update.php
@@ -0,0 +1,57 @@
+<?php
+include"config.php";
+$id=$_GET['edit'];
+$query ="SELECT *FROM tb_note WHERE id_note='$id'";
+$sql = mysqli_query($conn, $query);
+$result=mysqli_fetch_assoc($sql);
+
+
+// var_dump($result);
+// die();
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>notepad</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="container">
+        <h3 class="text-primary">
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-journal-bookmark" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M6 8V1h1v6.117L8.743 6.07a.5.5 0 0 1 .514 0L11 7.117V1h1v7a.5.5 0 0 1-.757.429L9 7.083 6.757 8.43A.5.5 0 0 1 6 8z"/>
+                <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
+                <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
+              </svg>
+              EDIT catatan pribadi
+        </h3>
+        <div class="card shadow bg-body-teriary rounded mb-4">
+            <div class="card-body">
+                <form class="d-flex" action="updateupdate.php" method="POST">
+                <a href="index.php" type="button" class="btn-close me-2 mt-1" disabled aria-label="Close"></a>
+                    <input class="form-control me-2" name="title_note" placeholder="JUDUL..." value="<?php echo $result['title_note']; ?>">
+                    <input class="form-control me-2" name="note_note" placeholder="WRITE HERE..." value="<?php echo $result['note_note']; ?>">
+                    <input type="hidden" name="id" value="<?php echo $result['id_note']; ?>">
+                    <button class="btn btn-primary" type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+</svg>
+                    </button>
+                </form>
+                     
+                      </div>
+                    
+                  </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
